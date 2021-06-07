238b34896368e1b6053b48b69cc810c72b8f42cb
HATZ01 <noah.hatz01@htl-salzburg.ac.at>
2021-06-07 09:36:18 +0200

    Add basket Object. Interaction between flower and vase - egg now drops when flower enters vase

---
+++ Assets/Scripts/Vase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vase : MonoBehaviour
+{
+    public GameObject egg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.gameObject.name == "Flowers")
+        {
+            Debug.Log("Flowers");
+            egg.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
