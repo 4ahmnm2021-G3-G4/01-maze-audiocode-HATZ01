4cd08b902efecfa814e89d8120770407507f87ea
HATZ01 <noah.hatz01@htl-salzburg.ac.at>
2021-06-14 12:23:28 +0200

    Add Interaction between door and egg

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    public GameObject door;
+    public Vector3 target = new Vector3(1.953262f, 3.338906f, 0.1712306f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.gameObject.name == "Egg")
+        {
+            door.transform.position = target;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
