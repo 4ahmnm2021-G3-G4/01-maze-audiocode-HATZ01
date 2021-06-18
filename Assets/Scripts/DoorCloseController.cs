33c11c9c8afb3790e603475b4cd3fdc99d63bc04
HATZ01 <noah.hatz01@htl-salzburg.ac.at>
2021-06-18 18:03:02 +0200

    Add trigger for door closing.

---
+++ Assets/Scripts/DoorCloseController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCloseController : MonoBehaviour
+{
+    public GameObject door;
+    public Vector3 target = new Vector3();
+    public AudioSource audioData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.gameObject.name == "Player")
+        {
+            door.transform.position = target;
+            audioData.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
