008cd72f3ef0030a199082173c263aeb8f254afc
HATZ01 <noah.hatz01@htl-salzburg.ac.at>
2021-06-18 18:10:30 +0200

    Add sound for eggs.

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -9,10 +9,13 @@ public class DoorController : MonoBehaviour
     public Vector3 target = new Vector3(1.953262f, 3.338906f, 0.1712306f);
     public AudioSource audioData;
 
+    public AudioSource audioData2;
+
     // Start is called before the first frame update
     void Start()
     {
         audioData = GetComponent<AudioSource>();
+        audioData2 = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider trigger)
@@ -21,6 +24,7 @@ public class DoorController : MonoBehaviour
         {
             door.transform.position = target;
             audioData.Play();
+            audioData2.Play();
         }
     }
 }
